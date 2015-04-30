379ebfdc0c30561134923b70747bb9738e4308f0
kangkyu <kangkyu1111@gmail.com>
2015-04-30 14:00:18 -0700

    add all events button with count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def all_events
+    @all_events ||= Event.all
+  end
+  helper_method :all_events
 end
