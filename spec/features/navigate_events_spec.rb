8e14073ba5536fa746c62adba6716f09a7ad4bef
kangkyu <kangkyu1111@gmail.com>
2015-08-04 17:37:25 -0700

    add show page of task, and link to it from event show page

--- spec/features/navigate_events_spec.rb
+++ spec/features/navigate_events_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 describe "Event navigation" do
   
   it "allows navigation from the show page to index listing" do
-    event = Event.create(event_name: "Coffee Conference",
+    event = Event.create([{event_name: "Coffee Conference",
                           venue: "Los Angeles",
                           description: "Local Roasters bring out and make cups of coffee over and over",
-                          date: "2018-05-02")
+                          date: "2018-05-02"},{event_name: "Movie Night",
+                          venue: "Burbank",
+                          description: "Come and Watch three movies with price of one movie",
+                          date: "2015-12-15"}])
 
     visit event_url(event)
-    click_link "All Events"
+    click_link "2 Events"
     expect(current_path).to eq(events_path)
   end
 
